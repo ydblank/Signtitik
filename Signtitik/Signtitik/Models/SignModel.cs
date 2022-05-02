e3e173ab0824ed35223f88937f9a8a8d90d1aea4
Frooge <jaantaros@gmail.com>
2022-05-02 16:37:45 +0800

    Add sign model

---
+++ Signtitik/Signtitik/Models/SignModel.cs
@@ -0,0 +1,20 @@
+﻿using Signtitik.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Signtitik.Models
+{
+    class SignModel : ObservableObject
+    {
+        string _name { get; set; }
+        string _category { get; set; }
+        string _gif { get; set; }
+        string _icon { get; set; }
+
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged("Name"); } }
+        public string Category { get { return _category; } set { _category = value; OnPropertyChanged("Category"); } }
+        public string Gif { get { return _gif; } set { _gif = value; OnPropertyChanged("Gif"); } }
+        public string Icon { get { return _icon; } set { _icon = value; OnPropertyChanged("Icon"); } }
+    }
+}
