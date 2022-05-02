54d474c67442a608495b92d64f33b911d0093ab9
therealmai <jomar35leano@gmail.com>
2022-05-02 17:29:42 +0800

    Splashscreen activity

---
+++ Signtitik/Signtitik.Android/SplashActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Signtitik.Droid
+{
+    [Activity(Label = "Signtitik", Icon = "@mipmap/ic_launcher", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
